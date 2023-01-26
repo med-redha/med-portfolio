6f1e7eb0d39d3f079700a6a449291aefdd244bb6
med-redha <redhaboutahir@gmail.com>
2023-01-26 03:18:21 +0100

    feat: edit data

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -74,7 +74,7 @@ export default {
           img: tml,
           title: "About Me",
           description:
-            "Hi! I am Redha Boutahir, a web designer/developer focused on crafting great web experiences. Designing and Coding have been my passion since the days I started working with computers but I found myself into web design/development since 2007. I enjoy creating beautifully designed, intuitive and functional websites.",
+            "Hi! I am Redha Boutahir, a web designer/developer focused on crafting great web experiences. Designing and Coding have been my passion since the days I started working with computers but I found myself into web design/development since 2022. I enjoy creating beautifully designed, intuitive and functional websites.",
         },
       ],
       
